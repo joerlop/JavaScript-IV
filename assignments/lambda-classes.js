9c108a04ab280b911e29cb0c7c3b32d21484a7e2
joerlop <jj.erlich155@gmail.com>
2019-04-04 14:53:40 -0500

    Built Person, Instructor and Student constructors

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,61 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(pAttributes) {
+        this.name = pAttributes.name;
+        this.age = pAttributes.age;
+        this.location = pAttributes.location;
+        this.gender = pAttributes.gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(iAtts) {
+        super(iAtts);
+        this.specialty = iAtts.specialty;
+        this.favLanguage = iAtts.favLanguage;
+        this.catchPhrase = iAtts.catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person {
+    constructor(sAtts) {
+        super(sAtts);
+        this.previousBackground = sAtts.previousBackground;
+        this.className = sAtts.className;
+        this.favSubjects = sAtts.favSubjects;
+    }
+
+    listsSubjects() {
+        for (let subject of this.favSubjects) {
+            console.log(subject);
+        }
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor (pmAtts) {
+        super(pmAtts);
+        this.gradClassName = pmAtts.gradClassName;
+        this.favInstructor = pmAtts.favInstructor;
+    }
+}
